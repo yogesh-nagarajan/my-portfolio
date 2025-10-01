cf1320bd6a0b36dcefdfe369a8c1ccff4be5fca5
Yogesh N <yogeshn4120@gmail.com>
2025-10-01 14:24:52 +0530

    fixed the correct routes

--- src/main.jsx
+++ src/main.jsx
@@ -12,7 +12,7 @@ import "./index.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename="/my-portfolio">
         <Routes>
           <Route path="/" element={<App />}>
             <Route index element={<Home />} />
